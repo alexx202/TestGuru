5404f7fc1963d038cfb41ff44f7bdf3c0427211a
alex <nuum11@mail.ru>
2022-05-11 23:49:36 +0300

    feat add test model and migration

---
+++ db/migrate/20220511204417_create_tests.rb
@@ -0,0 +1,11 @@
+class CreateTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, default: 1, null: false
+      t.integer :category, null: false
+
+      t.timestamps
+    end
+  end
+end

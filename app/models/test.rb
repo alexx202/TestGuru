7003703f8755097bedf71d577e158d90f7ed3e5b
alex <nuum11@mail.ru>
2022-06-14 17:39:31 +0300

    feat: add UserTest model and methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.titles_tests(category)
+    Test.where(category_id: Category.find_by!(title: category).id).order(title: :desc).pluck(:title)
+  end
+
 end

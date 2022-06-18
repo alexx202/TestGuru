9fefa7e0217be534e5fc93f7cf383b6e4fffc39d
alex <nuum11@mail.ru>
2022-06-18 20:53:47 +0300

    feat: add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+
   def self.titles_tests(category)
     joins('JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: category })
--- db/seeds.rb
+++ db/seeds.rb
@@ -100,7 +100,7 @@ answers = Answer.create!(
   ]
 )
 
-UserTest.create!(
+TestsUser.create!(
   [
     { user_id: 1, test_id: 1 },
     { user_id: 1, test_id: 2 },

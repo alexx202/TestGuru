fb8c39c675e74028af45923f798148c129058bd8
alex <nuum11@mail.ru>
2022-06-24 21:50:55 +0300

    fix: add dependent

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
 
-  has_many :questions
-  has_many :tests_users
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
   def self.titles_tests(category)

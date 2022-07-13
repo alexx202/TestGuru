7920d1061a59bdcf4bb01ea2601f84338a84c88b
alex <nuum11@mail.ru>
2022-07-13 21:02:54 +0300

    feat: add test_passages assets, test_passages_controller methods

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,14 +1,9 @@
 class TestPassagesController < ApplicationController
+  before_action :find_test_passage, only: %i[show result update]
 
-  before_action :set_test_passage, only: %i[show result update]
+  def show; end
 
-  def show
-
-  end
-
-  def result
-
-  end
+  def result; end
 
   def update
     @test_passage.accept!(params[:answer_ids])
@@ -22,7 +17,7 @@ class TestPassagesController < ApplicationController
 
   private
 
-  def set_test_passage
+  def find_test_passage
     @test_passage = TestPassage.find(params[:id])
   end
 end

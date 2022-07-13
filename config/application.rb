0220e2c24093f097da8da379f21f4fd900fba357
alex <nuum11@mail.ru>
2022-07-13 09:17:26 +0300

    feat: add test_passages_contoller, changed models and views

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module TestGuru
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Moscow"
     config.i18n.available_locales = %i[en ru]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
   end
 end

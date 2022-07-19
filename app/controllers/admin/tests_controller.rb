5871f14406f5efbb5197a7fe40b4c2a28e9225d1
alex <nuum11@mail.ru>
2022-07-20 00:59:05 +0300

    feat: add admin_controller and admin_view

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,55 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_test, only: %i[show edit update destroy start]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = Test.new(create_params)
+
+    if @test.save
+      redirect_to tests_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(create_params)
+      redirect_to tests_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def create_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,42 +1,9 @@
 class TestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :find_test, only: %i[start]
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    @questions = @test.questions
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def edit; end
-
-  def create
-    @test = Test.new(create_params)
-
-    if @test.save
-      redirect_to tests_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @test.update(create_params)
-      redirect_to tests_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -48,9 +15,5 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def create_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end

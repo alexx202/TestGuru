9cc62ddea37aea68c47b3beeaf08191bb84c7d23
alex <nuum11@mail.ru>
2022-08-01 12:27:19 +0300

    fix: replace variable

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,11 +1,11 @@
 class GistsController < ApplicationController
   def create
     @test_passage = TestPassage.find(params[:test_passage_id])
-    @result = GistQuestionService.new(@test_passage.current_question).call
+    result = GistQuestionService.new(@test_passage.current_question).call
 
-    if @result.success?
-      create_gist
-      flash[:success] = "#{t('.success')} #{view_context.link_to('gist', @result.url)}"
+    if result.success?
+      create_gist(result)
+      flash[:success] = "#{t('.success')} #{view_context.link_to('gist', result.url)}"
     else
       flash[:danger] = t('.fail')
     end
@@ -15,7 +15,7 @@ class GistsController < ApplicationController
 
   private
 
-  def create_gist
-    @test_passage.user.gists.create(question_id: @test_passage.current_question.id, url: @result.url)
+  def create_gist(result)
+    @test_passage.user.gists.create(question_id: @test_passage.current_question.id, url: result.url)
   end
 end
